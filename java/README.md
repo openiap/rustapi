10588bff9a739a90a6f3d3d56bfdb3ab915be09e
skadefro <allan@openrpa.dk>
2025-02-11 19:36:49 +0100

    test using maven so we can build and debug from main project

---
+++ java/README.md
@@ -0,0 +1,14 @@
+# OpenIAP Java Client
+
+To build the project:
+```bash
+mvn clean package
+```
+
+To run the application:
+```bash
+# This will run the jar that includes all dependencies
+java -jar target/openiap-java-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+```
+
+Note: Make sure to use the `-jar-with-dependencies.jar` version of the jar file, as this contains all required dependencies. The regular jar file will not work correctly.
--- java/src/main/java/io/openiap/NativeLoader.java
+++ java/src/main/java/io/openiap/NativeLoader.java
@@ -7,13 +7,16 @@ public class NativeLoader {
         String arch = System.getProperty("os.arch");
         String os = System.getProperty("os.name").toLowerCase();
         String libPath = "";
+        // System.out.println("Current path" + System.getProperty("user.dir"));
+        // if System.getProperty("user.dir") does not ends with "java" then we are in the root directory, then add "java" to the path
+        String base = System.getProperty("user.dir").endsWith("java") ? "" : "java/";
 
         if (os.contains("win")) {
-            libPath = "lib/openiap-windows-" + (arch.contains("64") ? "x64" : "i686") + ".dll";
+            libPath = base + "lib/openiap-windows-" + (arch.contains("64") ? "x64" : "i686") + ".dll";
         } else if (os.contains("mac")) {
-            libPath = "lib/libopeniap-macos-" + (arch.contains("64") ? "x64" : "arm64") + ".dylib";
+            libPath = base + "lib/libopeniap-macos-" + (arch.contains("64") ? "x64" : "arm64") + ".dylib";
         } else if (os.contains("linux")) {
-            libPath = "lib/libopeniap-linux-" + (arch.contains("64") ? "x64" : "arm64") + ".so";
+            libPath = base + "lib/libopeniap-linux-" + (arch.contains("64") ? "x64" : "arm64") + ".so";
         }
 
         File localLib = new File(libPath);
