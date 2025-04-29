b765c7ccdb3c9be899b96afd81eaf38c2c6443b3
skadefro <allan@openrpa.dk>
2025-04-29 14:34:09 +0200

    update for 0.0.36

--- c/conanfile.py
+++ c/conanfile.py
@@ -4,7 +4,7 @@ import os
 
 class OpenIAPConan(ConanFile):
     name = "openiap"
-    version = "0.0.35"
+    version = "0.0.36"
     license = "MPL-2.0"
     url = "https://github.com/openiap/rustapi"
     homepage = "https://openiap.io"
